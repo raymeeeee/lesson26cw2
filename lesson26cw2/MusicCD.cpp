459e40130e023933a7cc3e273f476821d8a7737a
raymeeeee <daniapoluektov@gmail.com>
2024-09-23 17:01:15 +0100

    create Class MusicCD definition

--- lesson26cw2/MusicCD.cpp
+++ lesson26cw2/MusicCD.cpp
@@ -2,21 +2,27 @@
 
 MusicCD::MusicCD()
 {
+    m_duration = 0;
 }
 
 MusicCD::MusicCD(string name, string author, int year, float price, float duration)
+    : Item(name, author, year, price)
 {
+    m_duration = duration;
 }
 
 void MusicCD::setDuration(float duration)
 {
+    m_duration = duration;
 }
 
 float MusicCD::getDuration() const
 {
-    return 0.0f;
+    return m_duration;
 }
 
 void MusicCD::showMusicCD() const
 {
+    showItem();
+    cout << "Duration:" << m_duration << endl;
 }
