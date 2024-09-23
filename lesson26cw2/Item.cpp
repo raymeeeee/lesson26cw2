2521f37e1ff0c1fdd71293da45c1a64e7fb7f565
raymeeeee <daniapoluektov@gmail.com>
2024-09-23 16:51:50 +0100

    add class Item

--- lesson26cw2/Item.cpp
+++ lesson26cw2/Item.cpp
@@ -1,1 +1,53 @@
 #include "Item.h"
+
+Item::Item()
+{
+    m_name;
+    m_author;
+    m_year;
+    m_price;
+}
+
+Item::Item(string name, string author, int year, float price)
+{
+}
+
+void Item::setName(string name)
+{
+}
+
+void Item::setAuthor(string author)
+{
+}
+
+void Item::setYear(int year)
+{
+}
+
+void Item::setPrice(float price)
+{
+}
+
+string Item::getName() const
+{
+    return string();
+}
+
+string Item::getAuthor() const
+{
+    return string();
+}
+
+int Item::getYear() const
+{
+    return 0;
+}
+
+float Item::getPrice() const
+{
+    return 0.0f;
+}
+
+void Item::showItem() const
+{
+}
--- lesson26cw2/Item.h
+++ lesson26cw2/Item.h
@@ -5,5 +5,25 @@ using namespace std;
 
 class Item
 {
+protected:
+	string m_name;
+	string m_author;
+	int m_year;
+	float m_price;
+public:
+	Item();
+	Item(string name, string author, int year, float price);
+
+	void setName(string name);
+	void setAuthor(string author);
+	void setYear(int year);
+	void setPrice(float price);
+
+	string getName()const;
+	string getAuthor()const;
+	int getYear()const;
+	float getPrice()const;
+
+	void showItem()const;
 };
 
